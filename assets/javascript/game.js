e3e8fde1b69a2e5f337495159dec6328dfb57492
Shantell Davis <shantell87@gmail.com>
2019-05-30 22:18:51 -0700

    added click functions to crystals in js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+//crystal variables
+//scores (your score and target score)
+//win count and loss count
+//button clicks for each crystal
+$('#blue').click(function () {
+    alert("test");
+});
+$('#pink').click(function () {
+    alert("test");
+});
+$('#purple').click(function () {
+    alert("test");
+});
+$('#yellow').click(function () {
+    alert("test");
+});
